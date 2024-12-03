ad198866f3abca7bf970cc5098e7aa31676040f7
Sumit Gupta <sgupta98mnit@gmail.com>
2024-12-03 05:20:09 -0500

    Adding support for reading messages REPL(Reading messages continuously)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,17 +7,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-         System.out.print("$ ");
+        while(true) {
+            System.out.print("$ ");
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
 
-        CommandRegistry commandRegistry = CommandRegistry.getInstance();
-        try {
-            Command command = commandRegistry.getCommand(input);
-            command.execute();
-        } catch (CommandNotFound e) {
-            System.out.println(e.getMessage());
+            CommandRegistry commandRegistry = CommandRegistry.getInstance();
+            try {
+                Command command = commandRegistry.getCommand(input);
+                command.execute();
+            } catch (CommandNotFound e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
