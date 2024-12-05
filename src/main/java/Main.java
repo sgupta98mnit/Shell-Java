fdc21c95764b6655432ec311fd1b3496ed69c6d3
Sumit Gupta <sgupta98mnit@gmail.com>
2024-12-04 20:07:57 -0500

    debug Path command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
+        for(int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
         while(true) {
             System.out.print("$ ");
 
