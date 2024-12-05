6c56d1696bdfed9adbacadde2e3b2719d85b0f93
Sumit Gupta <sgupta98mnit@gmail.com>
2024-12-05 17:07:28 -0500

    handle case for invalid directory

--- src/main/java/commands/Cd.java
+++ src/main/java/commands/Cd.java
@@ -2,11 +2,19 @@ package commands;
 
 import exception.CommandNotFound;
 
+import java.io.File;
+
 public class Cd implements Command{
     @Override
     public void execute(CommandContext context) throws CommandNotFound {
         String path = context.getArgument();
 
-        System.setProperty("user.dir", path);
+        File file = new File(path);
+
+        if(!file.exists()){
+            System.setProperty("user.dir", path);
+        } else {
+            System.out.println(context.getLine() + ": No such file or directory");
+        }
     }
 }
