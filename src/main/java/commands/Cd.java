a696ece7180bc20ed84ada551fb5578d9ffe9f71
Sumit Gupta <sgupta98mnit@gmail.com>
2024-12-05 17:22:12 -0500

    fix logic for directory traversal in Cd command

--- src/main/java/commands/Cd.java
+++ src/main/java/commands/Cd.java
@@ -5,19 +5,24 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 
-public class Cd implements Command{
+public class Cd implements Command {
     @Override
     public void execute(CommandContext context) throws CommandNotFound {
         String path = context.getArgument();
-
         String currentDirectory = System.getProperty("user.dir");
-        File file = new File(currentDirectory, path);
-        if(StringUtils.startsWithIgnoreCase(path, "/")) {
-            file = new File(path);
+
+        String fullPath;
+        if (StringUtils.startsWithIgnoreCase(path, "/")) {
+            fullPath = path;
+        } else {
+            // Relative path: combine with the current directory
+            fullPath = new File(currentDirectory, path).getAbsolutePath();
         }
 
-        if(file.exists()){
-            System.setProperty("user.dir", file.getAbsolutePath());
+        File file = new File(fullPath);
+
+        if (file.exists() && file.isDirectory()) {
+            System.setProperty("user.dir", fullPath);
         } else {
             System.out.println("cd: " + context.getArgument() + ": No such file or directory");
         }
