001778bd3acac43417fe84df8a710e00c6ea5f57
Sumit Gupta <sgupta98mnit@gmail.com>
2024-12-23 16:38:41 -0500

    fix in type command

--- src/main/java/commands/Type.java
+++ src/main/java/commands/Type.java
@@ -10,15 +10,19 @@ public class Type implements Command {
     @Override
     public void execute(CommandContext context) {
         CommandRegistry registry = CommandRegistry.getInstance();
-
+        boolean isShellBuiltin = false;
         try {
             registry.getCommandByCommandName(context.getArguments()[0]);
-            System.out.println(context.getArguments()[0] + " is a shell builtin");
+            isShellBuiltin = true;
         } catch (CommandNotFound e) {
             // Check if command exist in PATH
             String filePath = Utility.checkFileExistsOnPath(context.getArguments()[0]);
             if (StringUtils.isNotBlank(filePath)) {
                 System.out.println(context.getArguments()[0] + " is " + filePath);
+                return ;
+            }
+            if(isShellBuiltin) {
+                System.out.println(context.getArguments()[0] + " is a shell builtin");
                 return ;
             }
             System.out.println(e.getMessage());
