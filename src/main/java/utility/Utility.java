15fe3142ae1b847a5d22670d3cbb1b2c8d94f3e9
Sumit Gupta <sgupta98mnit@gmail.com>
2024-12-23 22:09:19 -0500

    debug in execute command

--- src/main/java/utility/Utility.java
+++ src/main/java/utility/Utility.java
@@ -21,10 +21,6 @@ public final class Utility {
 
         for(String p : paths) {
             String filePath = p + "/" + fileName;
-            if(StringUtils.equalsIgnoreCase("my_exe", fileName)) {
-                System.out.println(filePath);
-                System.out.println(checkFileExists(filePath));
-            }
             if(Utility.checkFileExists(filePath)) {
                 return filePath;
             }
